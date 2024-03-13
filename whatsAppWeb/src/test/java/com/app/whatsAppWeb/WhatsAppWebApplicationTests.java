127987fa345988c827f3e3b2a5be01f06f2fb80d
Revati Dhajekar <revadhajekar1@gmail.com>
2024-03-13 21:12:32 +0530

    "Project Setup "

---
+++ whatsAppWeb/src/test/java/com/app/whatsAppWeb/WhatsAppWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.whatsAppWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsAppWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
